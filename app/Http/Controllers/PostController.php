e9d50c88de8d53718f72a5fecaf9a79c28c3fc9c
Alex Rodrigues <rodriguesalex793@gmail.com>
2023-12-27 18:59:13 -0300

    feat: add blade templates

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('posts.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return 'save post in the database';
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return view('posts.show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        return view('posts.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        // return redirect(route('posts.index'));
+        return 'save updated post in the database';
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        return 'delete post from the database';
+    }
+
+    /**
+     * Display a list of the resources from a specific user
+     */
+    public function user(string $id)
+    {
+        return view('posts.index');
+    }
+
+    public function toggleFollow(string $id)
+    {
+        return 'logic for toggle like/dislike functionality';
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,39 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
+                    <div class="mt-6">
+                        <div class="flex mx-2 mb-4 space-x-4 text-xl border-b border-gray-300">
+                            <div class="hover:text-indigo-600 py-2 cursor-pointer">
+                                Followers</div>
+
+                            <div class="hover:text-indigo-600 py-2 pl-2 cursor-pointer">
+                                Following</div>
+
+                            <div class="hover:text-indigo-600 py-2 pl-2 cursor-pointer">
+                                Liked Posts</div>
+                        </div>
+                        <div><b>People that follow you:</b>
+                            <ul>
+                                <li><a class="hover:text-stone-500" href="{{ route('posts.index') }}">John Smith</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div><b>People that you follow:</b>
+                            <ul class="space-y-4">
+                                <li><a class="hover:text-stone-500" href="{{ route('posts.user', 1) }}">Jane Doe</a><a
+                                        href="{{ route('toggleFollow', 1) }}"
+                                        class="ml-3 inline font-bold text-sm px-4 py-1 text-white rounded bg-blue-500 hover:bg-blue-600">
+                                        Unfollow</a></li>
+                            </ul>
+                        </div>
+                        <div><b>Posts you liked:</b>
+                            <ul>
+                                <li><a class="hover:text-stone-500" href="{{ route('posts.show', 1) }}">Liked post
+                                        title</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
