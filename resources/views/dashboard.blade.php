c10493ef54c33baa3537cd60d9c9424d61addd1f
Alex Rodrigues <rodriguesalex793@gmail.com>
2023-12-28 16:35:42 -0300

    feat: make dashboard tabs dynamic

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,24 +10,42 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
-                    <div class="mt-6">
+
+                    <div
+                        x-data="{ tab: 1 }"
+                        class="mt-6"
+                    >
                         <div class="flex mx-2 mb-4 space-x-4 text-xl border-b border-gray-300">
-                            <div class="hover:text-indigo-600 py-2 cursor-pointer">
+                            <div
+                                :class="{ 'text-indigo-600 border-b border-indigo-600': tab == 1 }"
+                                class="hover:text-indigo-600 py-2 cursor-pointer"
+                                @click="tab = 1"
+                            >
                                 Followers</div>
 
-                            <div class="hover:text-indigo-600 py-2 pl-2 cursor-pointer">
+                            <div
+                                :class="{ 'text-indigo-600 border-b border-indigo-600': tab == 2 }"
+                                class="hover:text-indigo-600 py-2 pl-2 cursor-pointer"
+                                @click="tab = 2"
+                            >
                                 Following</div>
 
-                            <div class="hover:text-indigo-600 py-2 pl-2 cursor-pointer">
+                            <div 
+                                :class="{ 'text-indigo-600 border-b border-indigo-600': tab == 3 }"
+                                class="hover:text-indigo-600 py-2 pl-2 cursor-pointer"
+                                @click="tab = 3"
+                            >
                                 Liked Posts</div>
                         </div>
-                        <div><b>People that follow you:</b>
+                        <div x-show="tab == 1">
+                            <b>People that follow you:</b>
                             <ul>
                                 <li><a class="hover:text-stone-500" href="{{ route('posts.index') }}">John Smith</a>
                                 </li>
                             </ul>
                         </div>
-                        <div><b>People that you follow:</b>
+                        <div x-show="tab == 2">
+                            <b>People that you follow:</b>
                             <ul class="space-y-4">
                                 <li><a class="hover:text-stone-500" href="{{ route('posts.user', 1) }}">Jane Doe</a><a
                                         href="{{ route('toggleFollow', 1) }}"
@@ -35,7 +53,8 @@
                                         Unfollow</a></li>
                             </ul>
                         </div>
-                        <div><b>Posts you liked:</b>
+                        <div x-show="tab == 3">
+                            <b>Posts you liked:</b>
                             <ul>
                                 <li><a class="hover:text-stone-500" href="{{ route('posts.show', 1) }}">Liked post
                                         title</a>
