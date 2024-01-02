24a42c304597b61108c94adae58d32702c1df0b8
Alex Rodrigues <rodriguesalex793@gmail.com>
2024-01-02 11:34:47 -0300

    feat: list all posts on home page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -12,7 +13,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        return view('posts.index');
+        return view('posts.index', [
+            'posts' => Post::paginate(3)
+        ]);
     }
 
     /**
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,27 +6,35 @@
         <p class="mb-8 text-gray-500">A blog created with Laravel and Tailwind CSS</p>
     </div>
 
-    <div class="my-14 flex flex-col md:flex-row">
-        <p class="mb-8 text-gray-500 mr-20">18/08/2023</p>
-        <div class="space-y-4">
-            <h1 class="text-3xl font-bold tracking-tighter">Post title</h1>
-            <p class="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint sunt culpa consectetur? Dolores distinctio ex mollitia, officia quasi exercitationem fugiat illo hic dolorem, aut enim, commodi labore nemo sit at!</p>
-            <p><a href="{{ route('posts.show', 1) }}" class="text-red-500 hover:text-red-900 mt-8">Read more</a></p>
-            <div class="flex">
-                <a href="{{ route('posts.edit', 1) }}" title="edit" class="mr-2 cursor-pointer">
-                    <x-microblog.images.edit-icon />
-                </a>
+    @foreach ($posts as $post)
+        <div class="my-14 flex flex-col md:flex-row">
+            <p class="mb-8 text-gray-500 mr-20">{{ $post->created_at->format('j M Y') }}</p>
+            <div class="space-y-4">
+                <h1 class="text-3xl font-bold tracking-tighter">{{ $post->title }}</h1>
+                <p class="text-gray-500">{{ Str::limit($post->content, 200, ' ...') }}</p>
+                <p><a href="{{ route('posts.show', $post->id) }}" class="text-red-500 hover:text-red-900 mt-8">Read more</a></p>
 
-                <form method="POST" action="{{ route('posts.destroy', 1) }}">
-                    @csrf
-                    @method('delete')
-                    
-                    <button type="submit" href="{{ route('posts.destroy', 1) }}"
-                        onclick="return confirm('Are you sure?')" title="delete" class="cursor-pointer">
-                        <x-microblog.images.delete-icon />
-                    </button>
-                </form>    
+                @if ($post->user->is(auth()->user()))
+                    <div class="flex">
+                        <a href="{{ route('posts.edit', $post->id) }}" title="edit" class="mr-2 cursor-pointer">
+                            <x-microblog.images.edit-icon />
+                        </a>
+
+                        <form method="POST" action="{{ route('posts.destroy', $post->id) }}">
+                            @csrf
+                            @method('delete')
+                            
+                            <button type="submit" href="{{ route('posts.destroy', $post->id) }}"
+                                onclick="return confirm('Are you sure?')" title="delete" class="cursor-pointer">
+                                <x-microblog.images.delete-icon />
+                            </button>
+                        </form>    
+                    </div>
+                @endif
             </div>
         </div>
-    </div>
+    @endforeach
+    <br>
+
+    {{ $posts->links() }}
 </x-micro-blog-layout>
